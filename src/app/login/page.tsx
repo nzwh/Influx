1fb8d1027b1d6ae4f135029dc89ac947c517c328
jms <74128476+jmslouis@users.noreply.github.com>
2023-06-21 00:52:26 +0800

    [Login]: Add temporary div

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import React from 'react';
+
 export default function Home() {
     return (
-      <h1>Test</h1>
+      <main className="flex flex-col w-screen">
+      
+      <div className="fixed top-0 left-0 z-[-1] w-screen h-screen bg-gradient-to-b from-zinc-100 to-zinc-300"></div>
+
+      <div className="flex justify-center items-center w-screen h-screen">
+        <div className="bg-white rounded-lg p-4 flex">
+          <section id="left" className="p-4">
+            <p>Left div</p>
+          </section>
+          <section id="right" className="p-4">
+            <p>Right div</p>
+          </section>
+        </div>
+      </div>
+
+      </main>
     )
 }
   
