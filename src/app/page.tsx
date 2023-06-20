99333a7c7cb2de970ae429f24ddb70040a87da77
nzwh <ivanisaiahserato@gmail.com>
2023-06-20 18:06:59 +0800

    Home: Add post component
    Added the template for posts, will only work if posts.json exists.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
+import React from 'react';
+import Post from './components/Post';
+import posts from './posts.json';
 
 export default function Home() {
+  
   return (
-    <main>
+    <main className="flex flex-col w-screen bg-gradient-to-b from-zinc-100 to-zinc-300">
+
+      <nav className="flex flex-row h-20 w-full justify-between items-center px-[8%]">
+        <h6 className="text-slate-950 font-bold text-md tracking-tighter">Influx</h6>
+        <div className="flex align-center gap-4">
+          <Image src="/icons/b-search.svg" alt="Vercel Logo" width={16} height={16} />
+          <Image src="/icons/b-msgsqr.svg" alt="Vercel Logo" width={15} height={16} />
+          <Image src="/icons/b-bell.svg" alt="Vercel Logo" width={15} height={16} />          
+
+          <Link href="/profile" className="flex flex-row">
+            <Image className="rounded-full ml-2" src="/avatars/temp.jpg" alt="Profile" width={24} height={24} />
+            <div className="w-2 h-2 bg-gray-700 rounded-full"></div>
+          </Link>
+        </div>
+      </nav>
+
+      <div className="flex flex-row gap-2 w-full h-full justify-center align-center">
+        <div id="leftarea" className="flex flex-col gap-2 h-full w-[38rem]">
+          {posts.map((post, index) => (
+            <Post key={index} {...post} />
+          ))}
+        </div>
+
+        <div id="rightarea" className="h-full w-[18rem]">
+        </div>
+      </div>
+      
     </main>
   )
 }
