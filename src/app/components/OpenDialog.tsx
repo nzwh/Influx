6c2b7c98289027abe40952fe09024587f515ae01
jaye <74174900+shiopao@users.noreply.github.com>
2023-06-23 03:24:41 +0800

    Dash: Add seller details to OpenDialog
    Added seller details and fixed some details in the OpenDialog component.

--- src/app/components/OpenDialog.tsx
+++ src/app/components/OpenDialog.tsx
@@ -23,15 +23,15 @@ const OpenDialog: React.FC<OpenDialogProps> = ({ post, onClose }) => {
   return (
     <main className="text-gray-950 fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50"> 
       <div className="flex flex-row gap-2 w-1/2 h-1/2">
-        <Image className="h-full w-[38rem] bg-white rounded-lg" src={post!.images} alt="Images"/>
+        <Image className="h-full w-[38rem] bg-white rounded-lg" src={post!.images} alt="Images" />
         <div id="rightarea" className="flex flex-col gap-2 h-full w-[18rem">
-          <div className="flex flex-col gap-5 bg-white rounded-lg p-5">
+          <div className="flex flex-col gap-5 bg-white rounded-lg p-6">
             <div className="flex flex-row justify-between">
               <div className="flex flex-row gap-2 cursor-pointer">
                 <h6 className="text-gray-950 font-bold text-xs tracking-tighter leading-4">Return to post</h6>
-                <Image src="/icons/b-chvrg.svg" alt="Return to post" width={14} height={14}/>
+                <Image src="/icons/b-chvrg.svg" alt="Return to post" width={14} height={14} />
               </div>
-              <Image src="/icons/b-x.svg" alt="Close" width={14} height={14} onClick={onClose} className="cursor-pointer"/>
+              <Image src="/icons/b-x.svg" alt="Close" width={14} height={14} onClick={onClose} className="cursor-pointer" />
             </div>
             <div className="flex flex-row gap-2">
               <h1 className="text-gray-950 font-bold text-3xl tracking-tighter leading-4">${post!.price}</h1>
@@ -43,11 +43,11 @@ const OpenDialog: React.FC<OpenDialogProps> = ({ post, onClose }) => {
             <p className="text-gray-800 font-medium text-sm tracking-tighter leading-5 truncate break h-auto whitespace-pre-line">
 					  {post!.description}
 				    </p>
-            <div className="flex flex-row items-center gap-14">
+            <div className="flex flex-row items-center justify-between">
               <div className="flex flex-row gap-2">
                 <Image src="/icons/b-arrup.svg" alt="Upvote Button" width={14} height={14} className="cursor-pointer"/>  
                 <h6 className="text-gray-950 font-bold text-sm tracking-tighter leading-4">{post!.upvotes - post!.downvotes}</h6>
-                <Image src="/icons/b-arrdw.svg" alt="Upvote Button" width={14} height={14} className="cursor-pointer"/> 
+                <Image src="/icons/b-arrdw.svg" alt="Upvote Button" width={14} height={14} className="cursor-pointer" /> 
               </div>
               <div className="flex flex-row gap-2 cursor-pointer">
                 <Image src="/icons/b-cart.svg" alt="Add to cart" width={14} height={14} />  
@@ -63,13 +63,40 @@ const OpenDialog: React.FC<OpenDialogProps> = ({ post, onClose }) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-2 bg-white rounded-lg p-4">
-          
+
+          <div className="flex flex-col gap-2 bg-white rounded-lg p-6">
+            <div className="flex flex-row gap-10">
+              <div className="flex flex-row gap-2">
+                <Image className="rounded-full cursor-pointer" src={post!.user_icon} alt="User Icon" width={40} height={40} />
+                <div className="flex flex-col">
+                  <div className="flex flex-row">
+                    <h6 className="text-gray-950 font-bold text-md tracking-tighter leading-5 cursor-pointer">{post!.user_name}</h6>
+                  </div>
+                  <div className="flex flex-row">
+                    <div className="flex flex-row gap-0.5">
+                      <h6 className="text-gray-500 font-bold text-xs tracking-tighter leading-4 cursor-pointer">{post!.user_handle}</h6>
+                      <span className="text-black font-extrabold text-[0.5rem] bg-gray-300 rounded-xl px-2 py-0.5 tracking-normal ml-2">SELLER</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex flex-row items-center gap-2">
+                  <Image src="/icons/b-map-t.svg" alt="Location" width={12} height={12} />
+                  <h6 className="text-gray-800 font-bold text-xs tracking-tighter leading-4">[User location]</h6>
+                </div>
+                <div className="flex flex-row items-center gap-2">
+                  <Image src="/icons/b-box.svg" alt="Delivery Method" width={12} height={12} />
+                  <h6 className="text-gray-800 font-bold text-xs tracking-tighter leading-4">[User MOD]</h6>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col gap-2 bg-white rounded-lg p-4">
+          
+          <div className="flex flex-col gap-2 bg-white rounded-lg p-6">
             <div className="flex flex-row justify-between">
               <h6 className="text-gray-950 font-bold text-sm tracking-tighter leading-4">Comments</h6>
-              <Image src="/icons/b-filter.svg" alt="Filter" width={14} height={14} />
+              <Image src="/icons/b-filter.svg" alt="Filter" width={14} height={14} className="cursor-pointer" />
             </div>
           </div>
         </div>
