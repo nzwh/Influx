765d0a466bef2c06cca82e0c832d2884589e325c
jaye <74174900+shiopao@users.noreply.github.com>
2023-06-23 02:57:58 +0800

    Dash: Add post details to OpenDialog
    Added panels and post details to the OpenDialog component.

--- src/app/components/OpenDialog.tsx
+++ src/app/components/OpenDialog.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import Image from 'next/image';
+import React from "react";
+import { useState } from "react";
+import Comment from "../components/Comment";
+import useNode from "../hooks/useNode";
+
 import { PostInterface } from "@/libraries/interfaces";
 
 interface OpenDialogProps {
@@ -5,12 +13,66 @@ interface OpenDialogProps {
   post: PostInterface | null;
 }
 
+
+const comments = {
+  id: 1,
+  items: []
+};
+
 const OpenDialog: React.FC<OpenDialogProps> = ({ post, onClose }) => {
   return (
     <main className="text-gray-950 fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50"> 
-      <div className="bg-white rounded-lg w-1/2 h-1/2">
-        <h1>{post!.header}</h1>
-        <button onClick={onClose} className="bg-white ">Close</button>
+      <div className="flex flex-row gap-2 w-1/2 h-1/2">
+        <Image className="h-full w-[38rem] bg-white rounded-lg" src={post!.images} alt="Images"/>
+        <div id="rightarea" className="flex flex-col gap-2 h-full w-[18rem">
+          <div className="flex flex-col gap-5 bg-white rounded-lg p-5">
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row gap-2 cursor-pointer">
+                <h6 className="text-gray-950 font-bold text-xs tracking-tighter leading-4">Return to post</h6>
+                <Image src="/icons/b-chvrg.svg" alt="Return to post" width={14} height={14}/>
+              </div>
+              <Image src="/icons/b-x.svg" alt="Close" width={14} height={14} onClick={onClose} className="cursor-pointer"/>
+            </div>
+            <div className="flex flex-row gap-2">
+              <h1 className="text-gray-950 font-bold text-3xl tracking-tighter leading-4">${post!.price}</h1>
+              <div className=" bg-slate-700 rounded-full px-2 py-0.5">
+                <h6 className="text-white font-bold text-[0.5rem]">{post!.negotiable ? "NEGOTIABLE" : "FIXED"}</h6>
+              </div>
+            </div>
+            <h1 className="text-gray-950 font-bold text-lg tracking-tighter leading-5">{post!.header}</h1>
+            <p className="text-gray-800 font-medium text-sm tracking-tighter leading-5 truncate break h-auto whitespace-pre-line">
+					  {post!.description}
+				    </p>
+            <div className="flex flex-row items-center gap-14">
+              <div className="flex flex-row gap-2">
+                <Image src="/icons/b-arrup.svg" alt="Upvote Button" width={14} height={14} className="cursor-pointer"/>  
+                <h6 className="text-gray-950 font-bold text-sm tracking-tighter leading-4">{post!.upvotes - post!.downvotes}</h6>
+                <Image src="/icons/b-arrdw.svg" alt="Upvote Button" width={14} height={14} className="cursor-pointer"/> 
+              </div>
+              <div className="flex flex-row gap-2 cursor-pointer">
+                <Image src="/icons/b-cart.svg" alt="Add to cart" width={14} height={14} />  
+                <h6 className="text-gray-950 font-bold text-sm tracking-tighter leading-4">Add to cart</h6>
+              </div>
+              <div className="flex flex-row gap-2 cursor-pointer">
+                <Image src="/icons/b-share.svg" alt="Share" width={14} height={14} />  
+                <h6 className="text-gray-950 font-bold text-sm tracking-tighter leading-4">Share</h6>
+              </div>
+              <div className="flex flex-row gap-2 cursor-pointer">
+                <Image src="/icons/b-msgcrc.svg" alt="Message" width={14} height={14} />  
+                <h6 className="text-gray-950 font-bold text-sm tracking-tighter leading-4">Message</h6>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 bg-white rounded-lg p-4">
+          
+          </div>
+          <div className="flex flex-col gap-2 bg-white rounded-lg p-4">
+            <div className="flex flex-row justify-between">
+              <h6 className="text-gray-950 font-bold text-sm tracking-tighter leading-4">Comments</h6>
+              <Image src="/icons/b-filter.svg" alt="Filter" width={14} height={14} />
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
