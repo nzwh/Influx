36d5f5589a5b8033fa43b01a63492986f4caddd6
nzwh <ivanisaiahserato@gmail.com>
2023-06-26 21:33:44 +0800

    Add search results
    Loads posts based on a query from the route.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -206,7 +206,7 @@ export default function Profile() {
           </aside>
 
           <aside className="bg-white flex flex-col w-full rounded-sm p-4">
-            <h6 className="text-gray-800 font-regular text-[0.65rem] leading-[0.75rem]">About  •  Terms  •  Documentation  •  Legal influx.io © 2023.  Made with Next.js.</h6>
+            <h6 className="text-gray-800 font-regular text-[0.65rem] leading-[0.8rem]">About  •  Terms  •  Documentation  •  Legal<br/>influx.io © 2023.  Made with Next.js.</h6>
           </aside>
 
         </section>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,55 @@
-import React, { useState } from 'react';
-import Navbar from '@/src/app/components/Navbar';
+"use client"
 
-export default function Home() {
-    return (
-      <main className="flex flex-col w-screen">
-        <div className="fixed top-0 left-0 z-[-1] w-screen h-screen bg-gradient-to-b from-zinc-100 to-zinc-300"></div>
-      <Navbar />
-      
-      </main>
-    )
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation'
+import { PostInterface } from '@/libraries/interfaces';
+
+import Navbar from '@/src/app/components/Navbar';
+import PostTemplate from '@/src/app/components/PostTemplate';
+
+export default function Search() {
+
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = () => {
+      try {
+        const existingPosts: PostInterface[] = require('@/posts.json');
+        setPosts(existingPosts);
+      } catch (error) {
+        console.log('Error reading posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  let query : any = useSearchParams().get('q') || "";
+
+  return (
+    <main className="flex flex-col w-full">
+      <div className="fixed top-0 left-0 z-[-1] w-screen h-screen bg-gradient-to-b from-zinc-100 to-zinc-300"></div>
+    <Navbar />
+    <section id="wrapper" className="flex w-full h-full justify-center align-center py-20">
+      <section id="leftarea" className="flex flex-col gap-2 w-[32rem] h-full overflow-visible">
+
+        <section id="listings" className="w-full flex flex-row justify-between bg-white rounded-sm p-4 gap-4">
+          <h6 className="text-gray-800 font-regular text-xs leading-4">Showing results for "{query}".</h6>
+        </section>
+
+        <ul className="flex flex-col gap-2 h-full w-[32rem]">
+          {posts
+            .filter((post) => post.header.includes(query) || post.description.includes(query))
+            .map((post, index) => (
+            <li key={index}>
+              <PostTemplate key={index} {...post} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </section>
+    
+    </main>
+  )
 }
   
