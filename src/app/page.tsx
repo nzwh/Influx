49d46c49245d1ff64aded06c8575fad1ded06ec6
jms <74128476+jmslouis@users.noreply.github.com>
2023-07-10 11:02:35 +0800

    Update About panel into links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,7 +166,11 @@ export default function Home() {
             </aside>
             
             <aside className="bg-white flex flex-col w-full rounded-sm p-4">
-              <h6 className="text-gray-800 font-regular text-xs">About  •  Terms  •  Documentation  •  Legal
+              <h6 className="text-gray-800 font-regular text-xs">
+                <a href="/about" className="hover:underline">About</a> •&nbsp;
+                <a href="/terms" className="hover:underline">Terms</a> •&nbsp;
+                <a href="/documentation" className="hover:underline">Documentation</a> •&nbsp;
+                <a href="/legal" className="hover:underline">Legal</a>
               <br/>influx.io © 2023.  Made with Next.js.</h6>
             </aside>
 
