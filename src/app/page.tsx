feef8a7e8ab0f25f268a2c728057e1db975ea1a0
jaye <74174900+shiopao@users.noreply.github.com>
2023-07-19 23:55:39 +0800

    Implement delete post on database
    Implemented deleting posts on the database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,96 +25,100 @@ export default function Home() {
 
   // Renders existing posts on page load.
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        console.log('Fetching posts...');
-        const { data, error } = await supabase.from('posts').select('*');
-        if (error) {
-          throw error;
-        }
-        if (data) {
-          const formattedData = data.map((post) => ({
-            id: post.id,
-            origin: { uuid: post.origin } as CommunityInterface,
-            author: { uuid: post.author } as UserInterface,
-            type: post.type,
-            posted_at: post.posted_at,
-            price: post.price,
-            title: post.title,
-            description: post.description,
-            condition: post.condition,
-            tags: post.tags,
-            media: post.media,
-            is_edited: post.is_edited,
-            edited_at: post.edited_at,
-            upvotes: post.upvotes,
-            downvotes: post.downvotes,
-            shares: post.shares,
-            interests: post.interests,
-            bookmarks: post.bookmarks,
-            comments: post.comments,
-            is_open: post.is_open,
-            range: {
-              start: post.range_start,
-              end: post.range_end,
-            },
-          }));
-
-          const authorIds = formattedData.map((post) => post.author.uuid);
-          const { data: profiles, error: profilesError } = await supabase
-            .from('profiles')
-            .select('*')
-            .in('uuid', authorIds);
-
-          const communityIds = formattedData.map((post) => post.origin.uuid);
-          const { data: communities, error: communitiesError } = await supabase
-            .from('communities')
-            .select('*')
-            .in('uuid', communityIds);
-
-          formattedData.forEach((post) => {
-            const user = profiles?.find((user: UserInterface) => user.uuid === post.author.uuid);
-            const community = communities?.find((community: CommunityInterface) => community.uuid === post.origin.uuid);
-            user ? post.author = user : null;
-            community ? post.origin = community : null;
-          });
-
-          setPosts(formattedData);
-          console.log(data.length > 0 ? `Fetched ${data.length} posts.` : `'No posts found.'`)
-        }
-      } catch (error) {
-        console.log('Error fetching posts:', error);
-      }
-    };
-
     fetchPosts();
   }, []);
-  
-  // Handles adding new posts to the top of the list.
-  // TODO: Push to database
-  const handleAddPost = async (post: PostInterface) => {
-    /*try {
-      const { data, error } = await supabase.from('posts').insert([post]);
+
+  const fetchPosts = async () => {
+    try {
+      console.log('Fetching posts...');
+      const { data, error } = await supabase.from('posts').select('*');
       if (error) {
         throw error;
       }
       if (data) {
+        const formattedData = data.map((post) => ({
+          id: post.id,
+          origin: { uuid: post.origin } as CommunityInterface,
+          author: { uuid: post.author } as UserInterface,
+          type: post.type,
+          posted_at: post.posted_at,
+          price: post.price,
+          title: post.title,
+          description: post.description,
+          condition: post.condition,
+          tags: post.tags,
+          media: post.media,
+          is_edited: post.is_edited,
+          edited_at: post.edited_at,
+          upvotes: post.upvotes,
+          downvotes: post.downvotes,
+          shares: post.shares,
+          interests: post.interests,
+          bookmarks: post.bookmarks,
+          comments: post.comments,
+          is_open: post.is_open,
+          range: {
+            start: post.range_start,
+            end: post.range_end,
+          },
+        }));
+
+        const authorIds = formattedData.map((post) => post.author.uuid);
+        const { data: profiles, error: profilesError } = await supabase
+          .from('profiles')
+          .select('*')
+          .in('uuid', authorIds);
+
+        const communityIds = formattedData.map((post) => post.origin.uuid);
+        const { data: communities, error: communitiesError } = await supabase
+          .from('communities')
+          .select('*')
+          .in('uuid', communityIds);
+
+        formattedData.forEach((post) => {
+          const user = profiles?.find((user: UserInterface) => user.uuid === post.author.uuid);
+          const community = communities?.find((community: CommunityInterface) => community.uuid === post.origin.uuid);
+          user ? post.author = user : null;
+          community ? post.origin = community : null;
+        });
+
+        setPosts(formattedData);
+        console.log(data.length > 0 ? `Fetched ${data.length} posts.` : `'No posts found.'`)
+      }
+    } catch (error) {
+      console.log('Error fetching posts:', error);
+    }
+  };
+  
+  // Handles adding new posts to the top of the list.
+  // TODO: Push to database
+  const handleAddPost = async (post: PostInterface) => {
+    try {
+      console.log('Adding post...');
+      const { data, error } = await supabase.from('posts').insert([post]);
+      if (error) {
+        throw error;
+      }
+      if (data && data.length > 0) {
+        console.log('Post added successfully:', data[0]);
         setPosts((prevPosts) => [data[0], ...prevPosts]);
+        //fetchPosts();
       }
     } catch (error) {
       console.log('Error adding post:', error);
-    }*/
+    }
   };
 
   // Handles removing a post from the list.
   // TODO: Relocate to popup
-  // TODO: Push to database
   const handlePostDelete = async (postId: number) => {
     try {
+      console.log('Deleting post...');
       const { error } = await supabase.from('posts').delete().match({ id: postId });
       if (error) {
         throw error;
       }
+      console.log('Post deleted successfully.');
       const newPosts = posts.filter((post) => post.id !== postId);
       setPosts(newPosts);
     } catch (error) {
