48a0604c82ea519e8a350d5e4f66d23f930475ba
jms <74128476+jmslouis@users.noreply.github.com>
2023-07-24 01:40:31 +0800

    Add TrendingPanel

---
+++ src/app/backend/components/panels/columns/TrendingPanel.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+import Panel from '@/src/app/backend/components/template/PanelTemplate';
+
+const Trending: React.FC = () => {
+  return (
+    <Panel classes="p-4 gap-4">
+      
+    </Panel>
+  );
+};
+
+export default Trending;
