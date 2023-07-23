7f8100e7fb17563ecefbc05a75c21ec6c1c711bc
izzywincy <izabella_imperial@dlsu.edu.ph>
2023-07-23 23:45:30 +0800

    Fix Bookmark Page

---
+++ src/app/backend/components/panels/timeline/BookmarkPanel.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Panel from '@/src/app/backend/components/template/PanelTemplate';
+import { RotateCcw, Star } from 'lucide-react';
+
+interface Props {
+  handle: string;
+}
+
+const Bookmark: React.FC<Props> = ({ handle }) => {
+  return (
+    <Panel classes="flex-row p-4 gap-4">
+      <div className="flex flex-row gap-4 items-center">
+      <h6 className="text-gray-800 font-regular text-xs leading-4">Bookmarks by @{handle}</h6>
+      </div>
+    </Panel>
+  );
+};
+
+export default Bookmark;
