b50855ed73c5f991b96da7a9cb2ee337d5873ccd
jms <74128476+jmslouis@users.noreply.github.com>
2023-07-24 14:20:38 +0800

    Connect user info to pages

--- src/app/backend/components/panels/timeline/DashNewPostPanel.tsx
+++ src/app/backend/components/panels/timeline/DashNewPostPanel.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import CreatePostPopup from '@/src/app/backend/components/dialogs/CreatePostPopup';
 import Panel from '@/src/app/backend/components/template/PanelTemplate';
+import useFetchUser from "@/src/app/backend/hooks/useFetchUser";
 import { Post as PostInterface } from '@/libraries/structures';
 import { Glasses, Megaphone, Tag } from 'lucide-react';
 
@@ -11,6 +13,21 @@ interface Props {
 }
 
 const DashNewPost: React.FC<Props> = ({ onCreatePost }) => {
+  let activeD = JSON.parse(sessionStorage.getItem('token')!)
+  let router = useRouter();
+  
+  useEffect(() => {
+    if(sessionStorage.getItem('token')) {
+      activeD = JSON.parse(sessionStorage.getItem('token')!)
+      console.log(activeD.user.id)
+    }
+    else {
+      router.push('/home')
+    }
+  }, [])
+  
+  const { user, fetchUser} = useFetchUser({ type: 'userId', userId: activeD.user.id as string });
+  const activeData = user[0];
 
 	const [isCreatePostPopupOpen, setIsCreatePostPopupOpen] = useState(false);
 	const [postType, setPostType] = useState(0);
@@ -29,52 +46,56 @@ const DashNewPost: React.FC<Props> = ({ onCreatePost }) => {
 		onCreatePost(data);
   };
 
-  return (
-		<main>
-    <Panel classes="flex-row px-4 py-2 gap-4 cursor-pointer">
-		<div className="flex flex-row gap-3 items-center w-full" onClick={() => { 
-			handleCreatePostPopupOpen(); 
-			handlePostTypeInit(1); 
-		}}>
-			<Image className="rounded-full" src="/avatars/temp.jpg" alt="User Icon" width={22} height={22} />
-			<h6 className="text-gray-500 font-light text-xs">Post about something...</h6>
-		</div>
+  if (user && user.length > 0) {
+    return (
+      <main>
+      <Panel classes="flex-row px-4 py-2 gap-4 cursor-pointer">
+      <div className="flex flex-row gap-3 items-center w-full" onClick={() => { 
+        handleCreatePostPopupOpen(); 
+        handlePostTypeInit(1); 
+      }}>
+        <Image className="rounded-full" src={activeData.icon} alt="User Icon" width={22} height={22} />
+        <h6 className="text-gray-500 font-light text-xs">Post about something...</h6>
+      </div>
 
-		<div className="flex flex-row gap-1 items-center">
-			<div className="p-2 rounded-sm hover:bg-slate-200 transition-colors duration-200" 
-			onClick={() => { 
-				handleCreatePostPopupOpen(); 
-				handlePostTypeInit(1); 
-			}}>
-				<Megaphone className="text-gray-800" size={14}/>
-			</div>
+      <div className="flex flex-row gap-1 items-center">
+        <div className="p-2 rounded-sm hover:bg-slate-200 transition-colors duration-200" 
+        onClick={() => { 
+          handleCreatePostPopupOpen(); 
+          handlePostTypeInit(1); 
+        }}>
+          <Megaphone className="text-gray-800" size={14}/>
+        </div>
 
-			<div className="p-2 rounded-sm hover:bg-slate-200 transition-colors duration-200" 
-			onClick={() => { 
-				handleCreatePostPopupOpen(); 
-				handlePostTypeInit(2); 
-			}}>
-				<Glasses className="text-gray-800" size={14}/>
-			</div>
+        <div className="p-2 rounded-sm hover:bg-slate-200 transition-colors duration-200" 
+        onClick={() => { 
+          handleCreatePostPopupOpen(); 
+          handlePostTypeInit(2); 
+        }}>
+          <Glasses className="text-gray-800" size={14}/>
+        </div>
 
-			<div className="p-2 rounded-sm hover:bg-slate-200 transition-colors duration-200" 
-			onClick={() => { 
-				handleCreatePostPopupOpen(); 
-				handlePostTypeInit(3); 
-			}}>
-				<Tag className="text-gray-800" size={14}/>
-			</div>
-		</div>
-    </Panel>
+        <div className="p-2 rounded-sm hover:bg-slate-200 transition-colors duration-200" 
+        onClick={() => { 
+          handleCreatePostPopupOpen(); 
+          handlePostTypeInit(3); 
+        }}>
+          <Tag className="text-gray-800" size={14}/>
+        </div>
+      </div>
+      </Panel>
 
-		{isCreatePostPopupOpen && ( 
-			<CreatePostPopup isOpen={isCreatePostPopupOpen} onClose={() => { 
-				handleCreatePostPopupClose(); 
-				handlePostTypeInit(0); 
-			}} onSubmit={handleFormSubmit} passType={postType} />
-		)}
-		</main>
-  );
+      {isCreatePostPopupOpen && ( 
+        <CreatePostPopup isOpen={isCreatePostPopupOpen} onClose={() => { 
+          handleCreatePostPopupClose(); 
+          handlePostTypeInit(0); 
+        }} onSubmit={handleFormSubmit} passType={postType} />
+      )}
+      </main>
+    );
+  } else {
+    console.log("User data is not available yet.");
+  }
 };
 
 export default DashNewPost;
