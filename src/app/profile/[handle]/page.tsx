647860bff00410cec65db8728c3a29a199d211c3
jaye <74174900+shiopao@users.noreply.github.com>
2023-07-24 13:03:14 +0800

    Implement fetching user data into Profile
    Implemented fetching of user data into Profile. Unfinished.

---
+++ src/app/profile/[handle]/page.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import { useSearchParams, usePathname } from 'next/navigation';
+import Image from 'next/image';
+
+import TopbarNav from '@/src/app/backend/components/navigators/TopbarNav';
+import ExplorerNav from '@/src/app/backend/components/navigators/ExplorerNav';
+
+import useNavigateToProfile from '@/src/app/backend/hooks/useNavigateToProfile';
+import useFetchUser from "@/src/app/backend/hooks/useFetchUser";
+import useFetchPosts from "@/src/app/backend/hooks/useFetchPosts";
+import usePostActions from "@/src/app/backend/hooks/usePostActions";
+import Post from '@/src/app/backend/components/template/PostTemplate';
+import About from '@/src/app/backend/components/panels/columns/AboutPanel';
+import Background from '@/src/app/backend/components/panels/BackgroundPanel';
+
+import Listings from '@/src/app/backend/components/panels/timeline/ProfileListingsPanel';
+import ProfileDetails from '@/src/app/backend/components/panels/columns/ProfileDetailsPanel';
+import ProfileComments from '@/src/app/backend/components/panels/columns/ProfileCommentsPanel';
+import { Post as PostInterface } from '@/libraries/structures';
+
+export default function Profile() {
+
+  const [handleFromURL, setHandleFromURL] = useState('');
+  const pathName = usePathname();
+
+  const extractHandle = (pathName: string) => {
+    const parts = pathName.split('/');
+    return parts.length >= 3 && parts[1] === 'profile' ? parts[2] : '';
+  };
+
+  useEffect(() => {
+    const handle = extractHandle(pathName);
+    if (handle) {
+      console.log('Profile handle:', handle);
+      setHandleFromURL(handle);
+    }
+  }, [pathName]);
+
+  const { user, fetchUser } = useFetchUser({ type: 'handle', handle: handleFromURL });
+  const userData = user[0];
+
+  useEffect(() => {
+    if (userData) {
+      console.log('User data:', userData);
+    }
+  }, [userData]);
+
+  let activeUser = require('@/json/active.json'); // TODO: Load user info dynamically through auth
+  const { posts, fetchPosts } = useFetchPosts({ type: 'all' }); // TODO: Change type to user
+  const { handleAddPost, handleDeletePost } = usePostActions();
+  const [svg, setSvg] = useState('');
+
+  return (
+    <main>
+
+      {/* Templates */}
+      <Background />
+      <TopbarNav /> { /*// TODO: Add Create Post hook */ }
+
+      <div id="wrapper" className="flex flex-row gap-2 w-full h-full align-center py-20 px-[12%] wr-br justify-between">
+
+        {/* ExplorerNav & Padder */}
+        <ExplorerNav user={activeUser} wrapperClass="w-40 min-w-[10rem] ex-br" />
+        <div id="padder" className="w-40 min-w-[10rem] ex-br"></div>
+
+        <div className="flex flex-row gap-2 justify-center w-full ">
+
+          {/* New Post & Post Loader */}
+          <div className="flex flex-col gap-2 h-full overflow-y-visible w-[32rem] lg:mr-[16.5rem] z-50">
+            <Listings handle={userData.handle}/>
+            {posts && (
+              <ul className="flex flex-col gap-2 h-full w-[32rem] z-0">
+                {posts.map((post: PostInterface) => (
+                  <li key={post.id}>
+                    <Post post={post} onDelete={handleDeletePost} />
+                  </li>
+                ))}
+              </ul>
+            )}
+            {posts.length == 0 && (
+              <span className="flex flex-col items-center justify-center z-0">
+                <Image src={'/empty-illustration.png'} width={1000} height={1000} alt="No posts" className=" w-[50%]"/>
+                <p className='text-gray-700 text-sm'>No posts to show</p>
+              </span>
+            )}
+          </div>
+          
+          {/* Panels */}
+          <div className="flex flex-col gap-2 h-full fixed w-[16rem] ml-[32.5rem] ra-br">
+
+            <ProfileDetails />
+            <ProfileComments />
+            <About />
+
+          </div>
+        </div>
+
+        {/* Quick Access & Padder */}
+        <div id="quick" className="h-full w-40 min-w-[10rem] gap-4 flex flex-col fixed right-[12%] ex-br"></div>
+        <div id="padder" className="w-40 min-w-[10rem] ex-br"></div>
+      </div>
+    </main>
+  )
+}
