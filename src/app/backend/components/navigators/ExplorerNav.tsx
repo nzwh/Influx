445741cdb6a29a6932a0b352ea0f984058d838b9
nzwh <ivanisaiahserato@gmail.com>
2023-08-01 12:33:57 +0800

    Readd ExplorerNav elements
    Links in the ExplorerNav are now returned.

--- src/app/backend/components/navigators/ExplorerNav.tsx
+++ src/app/backend/components/navigators/ExplorerNav.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
-import useFetchUser from "@/src/app/backend/hooks/useFetchUser";
+import { User, Bookmark, Inbox, ShoppingBag, Settings, Sparkle, Truck, Moon, Megaphone, LayoutGrid } from 'lucide-react';
 import { UserClass } from '@/libraries/structures';
 
 interface WrapperProps {
@@ -33,6 +32,38 @@ const ExplorerNav: React.FC<WrapperProps> = ({ wrapperClass, user }) => {
         <h6 className="text-gray-500 font-regular text-[0.625rem] leading-4">{`@${user.handle}`}</h6>
       </div>
     </Link>
+    <ul className="flex flex-col gap-0.5 relative right-2">
+      <Link href="/explore" className="flex flex-row items-center gap-2 text-gray-700 px-2 py-1.5 rounded-sm hover:bg-slate-300 transition-colors duration-200">
+        <Sparkle size={16} strokeWidth={3}/>
+        <h6 className="font-regular text-sm">Explore</h6>
+      </Link>
+      <Link href="/bookmarks" className="flex flex-row items-center gap-2 text-gray-700 px-2 py-1.5 rounded-sm hover:bg-slate-300 transition-colors duration-200">
+        <Bookmark size={16} strokeWidth={3}/>
+        <h6 className="font-regular text-sm">Bookmarks</h6>
+      </Link>
+      <Link href="/cart" className="flex flex-row items-center gap-2 text-gray-700 px-2 py-1.5 rounded-sm hover:bg-slate-300 transition-colors duration-200">
+        <ShoppingBag size={16} strokeWidth={3}/>
+        <h6 className="font-regular text-sm">Shopping Cart</h6>
+      </Link>
+      <hr className="my-1"/>
+      <Link href="/theme" className="flex flex-row items-center gap-2 text-gray-700 px-2 py-1.5 rounded-sm hover:bg-slate-300 transition-colors duration-200">
+        <Moon size={16} strokeWidth={3}/>
+        <h6 className="font-regular text-sm">Theme</h6>
+      </Link>
+      <Link href="/profile" className="flex flex-row items-center gap-2 text-gray-700 px-2 py-1.5 rounded-sm hover:bg-slate-300 transition-colors duration-200">
+        <User size={16} strokeWidth={3}/>
+        <h6 className="font-regular text-sm">Profile</h6>
+      </Link>
+      <Link href="/settings" className="flex flex-row items-center gap-2 text-gray-700 px-2 py-1.5 rounded-sm hover:bg-slate-300 transition-colors duration-200">
+        <Settings size={16} strokeWidth={3}/>
+        <h6 className="font-regular text-sm">Settings</h6>
+      </Link>
+      {/* <hr />
+      <li className="flex flex-row items-center gap-2 text-gray-700">
+        <LayoutGrid size={16} strokeWidth={3}/>
+        <h6 className="font-regular text-sm">More</h6>
+      </li> */}
+    </ul>
     </section>
   );
 };
