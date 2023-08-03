00e95826dee367d45f6119eb891c5856bffec51f
nzwh <ivanisaiahserato@gmail.com>
2023-08-03 22:24:06 +0800

    Allow Supabase files
    Updated next.config.js to allow media from the supabase bucket.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'pmjwqjsoojzbascysdbk.supabase.co'
+    ],
+  }
+}
 
 module.exports = nextConfig
