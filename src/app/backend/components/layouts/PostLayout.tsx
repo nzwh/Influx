ddad06abc4f512e5ec469f68900afba130b6f278
nzwh <ivanisaiahserato@gmail.com>
2023-08-07 11:00:46 +0800

    Fix errors

--- src/app/backend/components/layouts/PostLayout.tsx
+++ src/app/backend/components/layouts/PostLayout.tsx
@@ -1,4 +1,4 @@
-"use client" // * Uses interactable components
+                                                        "use client" // * Uses interactable components
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
